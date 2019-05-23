ff211f323655b1451c649bcc212956b4bb0ecc48
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-05-23 18:06:30 -0400

    Created NStageElevator.java
     - Added a new Elevator template class that is similar to the NSegmentArm template class

---
+++ src/main/java/com/otrobotics/otrlib/systems/manipulator/elevator/NStageElevator.java
@@ -0,0 +1,31 @@
+package com.otrobotics.otrlib.systems.manipulator.elevator;
+
+import java.util.List;
+
+import com.otrobotics.otrlib.sensors.OTRSensor;
+
+import com.otrobotics.otrlib.systems.OTRPIDController;
+import com.otrobotics.otrlib.systems.manipulator.OTRManipulator;
+import com.otrobotics.otrlib.systems.manipulator.ManipulatorController;
+import com.otrobotics.otrlib.systems.manipulator.OTRManipulator;
+
+public class NStageElevator extends OTRManipulator {
+
+    public NStageElevator (List<Integer> can_ids, List<OTRPIDController> PID_controllers, 
+                        List<OTRSensor> feedback_sensors, ManipulatorController controller_type) {
+        super(can_ids, PID_controllers, feedback_sensors, controller_type);
+    }
+
+    public void init () {
+
+    }
+
+    public ManipulatorController getManipulatorControllerType () {
+        return ManipulatorController.SRX;
+    }
+
+    public void setPIDSetpoint (int can_device, double setpoint) {
+        PID_motor_controllers.get(can_device).setPIDSetpoint(setpoint);
+    }
+
+}
