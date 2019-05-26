5c17f9d396bc0e4e39db1ecde8f5f8d01f97cd4b
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-05-26 13:25:39 -0400

    Create QuadEncoderShooter.java
     - Added the first shooter class

---
+++ src/main/java/com/otrobotics/otrlib/systems/velocity/shooter/QuadEncoderShooter.java
@@ -0,0 +1,35 @@
+package com.otrobotics.otrlib.systems.velocity.shooter;
+
+import java.util.List;
+
+import com.otrobotics.otrlib.sensors.OTRSensor;
+import com.otrobotics.otrlib.systems.OTRController;
+import com.otrobotics.otrlib.systems.OTRPIDController;
+import com.otrobotics.otrlib.systems.velocity.OTRVelocity;
+import com.otrobotics.otrlib.systems.velocity.VelocityPIDObject;
+
+public class QuadEncoderShooter extends OTRVelocity {
+
+    public QuadEncoderShooter (List<Integer> can_ids, List<OTRPIDController> PID_controllers, 
+    List<OTRSensor> feedback_sensors, OTRController controller_type) {
+        super(can_ids, PID_controllers, feedback_sensors, controller_type);
+    }
+
+    public void init () {
+
+    }
+
+    public OTRController getManipulatorControllerType () {
+        return OTRController.SRX;
+    }
+
+    public void setPIDSetpoint (int can_device, double setpoint) {
+        for (VelocityPIDObject p : PID_motor_controllers) {
+            if (p.getDeviceID() == can_device) {
+                p.setPIDSetpoint(setpoint);
+                break;
+            }
+        }
+    }
+
+}
