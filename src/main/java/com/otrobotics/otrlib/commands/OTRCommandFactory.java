6c9a1fba65300c370c4fd40356e15793c881aad2
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-06-08 09:30:37 -0400

    Update OTRCommandFactory.java
     - Added elevator facoty build statement

--- src/main/java/com/otrobotics/otrlib/commands/OTRCommandFactory.java
+++ src/main/java/com/otrobotics/otrlib/commands/OTRCommandFactory.java
@@ -3,6 +3,7 @@ package com.otrobotics.otrlib.commands;
 import com.otrobotics.otrlib.commands.OTRCommand;
 import com.otrobotics.otrlib.systems.OTRSubsystem;
 import com.otrobotics.otrlib.systems.manipulator.arm.NSegmentArm;
+import com.otrobotics.otrlib.systems.manipulator.elevator.NStageElevator;
 import com.otrobotics.otrlib.commands.pid.*;
 
 public class OTRCommandFactory {
@@ -10,9 +11,12 @@ public class OTRCommandFactory {
     public static OTRCommand buildPIDCommand (String subsystem_id, OTRSubsystem subsystem,
                                               int[] can_ids, double[] setpoints) {
         if (subsystem_id.equals("ARM")) {
-                return new MoveArmPIDCommand((NSegmentArm)subsystem, can_ids, setpoints);
+            return new MoveArmPIDCommand((NSegmentArm)subsystem, can_ids, setpoints);
         }
 
+        if (subsystem_id.equals("ELEVATOR")) {
+            return new MoveElevatorPIDCommand((NStageElevator)subsystem, can_ids, setpoints);
+        }
         
         // Return empty stub
         return new OTRCommand(){
